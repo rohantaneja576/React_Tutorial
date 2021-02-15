2e3136a58f836668ecda3e70770bd22cf3c3c231
Rohan Taneja <rohantaneja576@gmail.com>
2021-02-16 01:22:02 +0530

    Initial structure added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tab_Switch from "./Components/Tab_Container/Tab_Switch";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Tab_Switch />
+    </div>
+  );
+};
+
+export default App;
